814a8615bc3f5b75de6cc74cf6e52a167ba5eb20
Censuradho <gustavoleiteoliveira800@gmail.com>
2023-02-23 09:26:17 -0300

    feat(curriculum): add education dto

--- backend/src/features/curriculum/curriculum.service.ts
+++ backend/src/features/curriculum/curriculum.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/database/prisma.service";
+import { CreateCurriculumDto } from "./dto/create";
 
 @Injectable()
 export class CurriculumService {
   constructor (private readonly prisma: PrismaService) {}
 
-  async create () {}
+  async create (payload: CreateCurriculumDto) {
+    
+  }
 }
--- backend/src/features/curriculum/dto/create.ts
+++ backend/src/features/curriculum/dto/create.ts
@@ -1,4 +1,83 @@
-import { IsBoolean, IsOptional, IsString } from "class-validator";
+import { Type } from "class-transformer";
+import { IsArray, IsBoolean, IsDateString, IsEnum, IsNotEmpty, IsOptional, IsString, MinLength, ValidateNested } from "class-validator";
+
+enum EducationLevel {
+  /*Ensino médio**/
+  highSchool = 'highSchool',
+  /*Ensino superior**/
+  universityEducation = 'universityEducation',
+  /*Curso online**/
+  onlineCourse = 'onlineCourse',
+  /*Curso presencial**/
+  classroomCourse = 'classroomCourse',
+  /*Certificação**/
+  certification = 'certification',
+  /*Evento de formação**/
+  trainingEvent = 'trainingEvent',
+  /*Tecnólogo**/
+  technologist = 'technologist',
+  /*Técnico**/
+  technician = 'technician',
+  /*Doutorado**/
+  doctorateDegree = 'doctorateDegree',
+  /*Pós-graduação**/
+  postgraduateStudies = 'postgraduateStudies',
+  /*Mestrado**/
+  master = 'master',
+  /*PHD**/
+  phD = 'phD',
+  /*Especialização**/
+  specialization = 'specialization',
+  /*Capacitação**/
+  training = 'training',
+  /*Aperfeiçoamento**/
+  improvement = 'improvement',
+  /*Reciclagem**/
+  recycling = 'recycling',
+  /*Profissionalizante**/
+  professionalizing = 'professionalizing',
+  /*Extensão**/
+  extension = 'extension',
+  /*Intercambio**/
+  exchange = 'exchange'
+}
+
+enum SituationEducation {
+  /*Não informar**/
+  notInform = 'notInform',
+  /*Completo**/
+  complete = 'complete',
+  /*Cursando**/
+  coursing = 'coursing',
+  /*Pausado**/
+  paused = 'paused',
+  /*Incompleto**/
+  incomplete = 'incomplete'
+}
+
+class Education {
+  @IsEnum(EducationLevel)
+  level: EducationLevel
+
+  @IsString()
+  institution_name: string
+
+  @IsString()
+  name: string
+
+  @IsEnum(SituationEducation)
+  situation: SituationEducation
+
+  @IsDateString()
+  initial_date: string
+
+  @IsDateString()
+  final_date: string
+
+  @IsOptional()
+  @IsBoolean()
+  is_main: boolean
+}
 
 export class CreateCurriculumDto {
   @IsString()
@@ -22,4 +101,8 @@ export class CreateCurriculumDto {
   @IsOptional()
   @IsBoolean()
   is_pcd?: string
+
+  @ValidateNested({ each: true })
+  @Type(() => Education)
+  education: Education[]
 }
