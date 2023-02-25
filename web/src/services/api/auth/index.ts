b4664c276a5c76602d773259f4aa63bcfb635744
Censuradho <gustavoleiteoliveira800@gmail.com>
2023-02-25 14:03:44 -0300

    feat(auth): implement me service

--- web/src/services/api/auth/index.ts
+++ web/src/services/api/auth/index.ts
@@ -1,4 +1,4 @@
-import { SignUpWithEmailPasswordRequest } from "./types";
+import { Me, SignInWithEmailPasswordRequest, SignInWithEmailPasswordResponse, SignUpWithEmailPasswordRequest } from "./types";
 
 import { api } from '@/services/api'
 
@@ -6,6 +6,16 @@ function signUpWithEmailPassword (payload: SignUpWithEmailPasswordRequest) {
   return api.post('/auth/sign-up', payload)
 }
 
+function signInWithEmailPassword (payload: SignInWithEmailPasswordRequest) {
+  return api.post<SignInWithEmailPasswordResponse>('/auth/login', payload)
+}
+
+function me () {
+  return api.get<Me>('/auth/me')
+}
+
 export const authService = {
-  signUpWithEmailPassword
+  signUpWithEmailPassword,
+  signInWithEmailPassword,
+  me
 }
