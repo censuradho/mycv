c181c935338d3fe9cae6d7d3e591b906f5953688
Censuradho <gustavoleiteoliveira800@gmail.com>
2023-02-27 13:26:04 -0300

    remove input file

--- web/src/components/common/input-file/index.tsx
+++
@@ -1,8 +0,0 @@
-import * as Styles from './styles'
-import { ComponentProps } from './types'
-
-export function Component (props: ComponentProps) {
-  return (
-    <Styles.Container></Styles.Container>
-  )
-}
